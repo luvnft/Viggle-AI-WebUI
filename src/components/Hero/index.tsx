6ae5de1b03123424878d8bbc773dc659208ed764
The Wizard of Hahz <info@luvnft.com>
2024-07-13 12:14:25 -0400

    updated

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -22,7 +22,7 @@ const Hero = () => {
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
-                    href="https://discord.gg/bYRwZQSe"
+                    href="https://discord.gg/dpGuGRyuu7"
                     className="px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out rounded-sm bg-primary hover:bg-primary/80"
                   >
                     {t('join')}
